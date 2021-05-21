4302c6ddb8dce954ae93758a0728deaf676d38d8
harshini1611 <84581325+harshini1611@users.noreply.github.com>
2021-05-21 20:37:47 +0530

    Add files via upload

--- config.php
+++ config.php
@@ -1,1 +1,14 @@
+<?php
+	$servername = 'localhost';
+	$user = 'root';
+	$pass = '';
+	$dbname = 'sparks';
 
+	$conn = mysqli_connect($servername,$user,$pass,$dbname);
+
+	if(!$conn){
+		die("Could Not Connect to the database".mysqli_connect_error());
+	}
+
+
+?>
